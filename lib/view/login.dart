8847cb20b7b168098e64bc47c308f78f1afbd5d6
Ronald Grant <grantrona@myvuw.ac.nz>
2022-10-04 14:08:10 +1300

    Login/Registration correctly scrolls using customScrollView when keyboard is onscreen

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -19,8 +19,6 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        resizeToAvoidBottomInset:
-            false, //prevents resizing of widgets from keyboard popup
         backgroundColor: globals.colorDark,
         appBar: AppBar(
           backgroundColor: globals.colorHighlight,
@@ -31,139 +29,146 @@ class _LoginState extends State<Login> {
 
         // Container for Welcome message
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                flex: 1,
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Icon(
-                        Icons.bluetooth_searching,
-                        size: 50,
-                        color: Colors.blue,
-                      ),
-                      Text(
-                        "FindMyDevice",
-                        style: globals.defaultFontTitle,
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.85,
-                  decoration: BoxDecoration(
-                    boxShadow: const [
-                      BoxShadow(
-                          color: Colors.black45,
-                          blurRadius: 25.0,
-                          spreadRadius: 5.0,
-                          offset: Offset(
-                            15.0,
-                            15.0,
-                          ))
-                    ],
-                    color: globals.colorLight,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 12),
-                        child: TextFormField(
-                          keyboardType: TextInputType.emailAddress,
-                          textInputAction: TextInputAction.next,
-                          controller: _emailController,
-                          decoration: InputDecoration(
-                              border: const UnderlineInputBorder(),
-                              labelText: 'Email',
-                              labelStyle: globals.defaultFontText,
-                              prefixIcon: const Icon(Icons.person),
-                              fillColor: globals.colorLight),
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.bluetooth_searching,
+                          size: 50,
+                          color: Colors.blue,
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 12),
-                        child: TextFormField(
-                          obscureText: true,
-                          controller: _passwordController,
-                          decoration: InputDecoration(
-                              border: const UnderlineInputBorder(),
-                              labelText: 'Password',
-                              labelStyle: globals.defaultFontText,
-                              prefixIcon: const Icon(Icons.lock),
-                              fillColor: globals.colorLight),
+                        Text(
+                          "FindMyDevice",
+                          style: globals.defaultFontTitle,
+                          textAlign: TextAlign.center,
                         ),
-                      ),
-                      TextButton(
-                        child: const Text(
-                          "Forgot password?",
-                          style: TextStyle(color: Colors.black54),
-                        ),
-                        onPressed: () {
-                          // TODO
-                        },
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 20),
-                        child: FractionallySizedBox(
-                          widthFactor: 0.8,
-                          child: ElevatedButton(
-                              style: ButtonStyle(
-                                  padding:
-                                      MaterialStateProperty.all<EdgeInsets>(
-                                          const EdgeInsets.all(15)),
-                                  backgroundColor: MaterialStateProperty.all(
-                                      globals.colorHighlight),
-                                  shape: MaterialStateProperty.all<
-                                          RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                  ))),
-                              onPressed: () {
-                                Auth().signIn(_emailController.text.trim(),
-                                    _passwordController.text.trim(), context);
-                              },
-                              child: Text(
-                                'Sign in',
-                                style: globals.buttonFontText,
-                              )),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/register');
-                  },
-                  child: Text(
-                    "New User? Register here!",
-                    style: GoogleFonts.lato(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w800,
-                      color: globals.colorHighlight,
+                      ],
                     ),
                   ),
                 ),
-              )
-            ],
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.85,
+                    decoration: BoxDecoration(
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.black45,
+                            blurRadius: 25.0,
+                            spreadRadius: 5.0,
+                            offset: Offset(
+                              15.0,
+                              15.0,
+                            ))
+                      ],
+                      color: globals.colorLight,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 12),
+                          child: TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            textInputAction: TextInputAction.next,
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                                border: const UnderlineInputBorder(),
+                                labelText: 'Email',
+                                labelStyle: globals.defaultFontText,
+                                prefixIcon: const Icon(Icons.person),
+                                fillColor: globals.colorLight),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 12),
+                          child: TextFormField(
+                            obscureText: true,
+                            controller: _passwordController,
+                            decoration: InputDecoration(
+                                border: const UnderlineInputBorder(),
+                                labelText: 'Password',
+                                labelStyle: globals.defaultFontText,
+                                prefixIcon: const Icon(Icons.lock),
+                                fillColor: globals.colorLight),
+                          ),
+                        ),
+                        TextButton(
+                          child: const Text(
+                            "Forgot password?",
+                            style: TextStyle(color: Colors.black54),
+                          ),
+                          onPressed: () {
+                            // TODO
+                          },
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 20),
+                          child: FractionallySizedBox(
+                            widthFactor: 0.8,
+                            child: ElevatedButton(
+                                style: ButtonStyle(
+                                    padding:
+                                        MaterialStateProperty.all<EdgeInsets>(
+                                            const EdgeInsets.all(15)),
+                                    backgroundColor: MaterialStateProperty.all(
+                                        globals.colorHighlight),
+                                    shape: MaterialStateProperty.all<
+                                            RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ))),
+                                onPressed: () {
+                                  Auth().signIn(_emailController.text.trim(),
+                                      _passwordController.text.trim(), context);
+                                },
+                                child: Text(
+                                  'Sign in',
+                                  style: globals.buttonFontText,
+                                )),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/register');
+                    },
+                    child: Text(
+                      "New User? Register here!",
+                      style: GoogleFonts.lato(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800,
+                        color: globals.colorHighlight,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
+            ]
         ),
       ),
+      )
     );
   }
 }
--- lib/view/register.dart
+++ lib/view/register.dart
@@ -20,152 +20,162 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset:
-            false, //prevents resizing of widgets from keyboard popup
-        backgroundColor: globals.colorDark,
-        appBar: AppBar(
-          backgroundColor: globals.colorHighlight,
-          title: const Text("Register"),
-          centerTitle: true,
-          titleTextStyle: globals.defaultFontHeader,
-        ),
+      backgroundColor: globals.colorDark,
+      appBar: AppBar(
+        backgroundColor: globals.colorHighlight,
+        title: const Text("Register"),
+        centerTitle: true,
+        titleTextStyle: globals.defaultFontHeader,
+      ),
 
-        // Container for Welcome message
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                flex: 2,
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Icon(
+      // Container for Welcome message
+      body: Center(
+        child: CustomScrollView(slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Icon(
                           Icons.bluetooth_searching,
                           size: 50,
                           color: Colors.blue,
                         ),
-                      Text(
-                        "FindMyDevice",
-                        style: globals.defaultFontTitle,
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
+                        Text(
+                          "FindMyDevice",
+                          style: globals.defaultFontTitle,
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                flex: 4,
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.85,
-                  decoration: BoxDecoration(
-                    boxShadow: const [
-                      BoxShadow(
-                          color: Colors.black45,
-                          blurRadius: 25.0,
-                          spreadRadius: 5.0,
-                          offset: Offset(
-                            15.0,
-                            15.0,
-                          )),
-                    ],
-                    color: globals.colorLight,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 12),
-                        child: TextFormField(
-                          keyboardType: TextInputType.emailAddress,
-                          textInputAction: TextInputAction.next,
-                          controller: _emailController,
-                          decoration: InputDecoration(
-                              border: const UnderlineInputBorder(),
-                              labelText: 'Email',
-                              labelStyle: globals.defaultFontText,
-                              prefixIcon: const Icon(Icons.person),
-                              fillColor: globals.colorLight),
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.85,
+                    decoration: BoxDecoration(
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.black45,
+                            blurRadius: 25.0,
+                            spreadRadius: 5.0,
+                            offset: Offset(
+                              15.0,
+                              15.0,
+                            )),
+                      ],
+                      color: globals.colorLight,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 12),
+                          child: TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            textInputAction: TextInputAction.next,
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                                border: const UnderlineInputBorder(),
+                                labelText: 'Email',
+                                labelStyle: globals.defaultFontText,
+                                prefixIcon: const Icon(Icons.person),
+                                fillColor: globals.colorLight),
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 12),
-                        child: TextFormField(
-                          obscureText: true,
-                          textInputAction: TextInputAction.next,
-                          controller: _passwordController,
-                          decoration: InputDecoration(
-                              border: const UnderlineInputBorder(),
-                              labelText: 'Password',
-                              labelStyle: globals.defaultFontText,
-                              prefixIcon: const Icon(Icons.lock),
-                              fillColor: globals.colorLight),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 12),
+                          child: TextFormField(
+                            obscureText: true,
+                            textInputAction: TextInputAction.next,
+                            controller: _passwordController,
+                            decoration: InputDecoration(
+                                border: const UnderlineInputBorder(),
+                                labelText: 'Password',
+                                labelStyle: globals.defaultFontText,
+                                prefixIcon: const Icon(Icons.lock),
+                                fillColor: globals.colorLight),
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 12),
-                        child: TextFormField(
-                          obscureText: true,
-                          controller: _confirmController,
-                          decoration: InputDecoration(
-                              border: const UnderlineInputBorder(),
-                              labelText: 'Confirm Password',
-                              labelStyle: globals.defaultFontText,
-                              prefixIcon: const Icon(Icons.lock),
-                              fillColor: globals.colorLight),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 12),
+                          child: TextFormField(
+                            obscureText: true,
+                            controller: _confirmController,
+                            decoration: InputDecoration(
+                                border: const UnderlineInputBorder(),
+                                labelText: 'Confirm Password',
+                                labelStyle: globals.defaultFontText,
+                                prefixIcon: const Icon(Icons.lock),
+                                fillColor: globals.colorLight),
+                          ),
                         ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 20),
-                        child: FractionallySizedBox(
-                          widthFactor: 0.8,
-                          child: ElevatedButton(
-                              style: ButtonStyle(
-                                padding: MaterialStateProperty.all<EdgeInsets>(
-                                  const EdgeInsets.all(15)
-                                ),
-                                backgroundColor: MaterialStateProperty.all(globals.colorHighlight),
-                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ))),
-                              onPressed: () {
-                                if (_emailController.text.isEmpty || _confirmController.text.isEmpty || _passwordController.text.isEmpty) {
-                                  showAlertDialog(context, "Please fill all the fields!");
-                                  return;
-                                }
-                                if (_confirmController.text != _passwordController.text) {
-                                  showAlertDialog(context, "Passwords do not match!");
-                                  _confirmController.clear();
-                                  _passwordController.clear();
-                                  return;
-                                }
-                                Auth().register(_emailController.text.trim(), _passwordController.text.trim(), context);
-                              },
-                              child: Text(
-                                'Register',
-                                style: globals.buttonFontText,
-                              )),
-                        ),
-                      )
-                    ],
+                        Container(
+                          margin: const EdgeInsets.only(top: 20),
+                          child: FractionallySizedBox(
+                            widthFactor: 0.8,
+                            child: ElevatedButton(
+                                style: ButtonStyle(
+                                    padding:
+                                        MaterialStateProperty.all<EdgeInsets>(
+                                            const EdgeInsets.all(15)),
+                                    backgroundColor: MaterialStateProperty.all(
+                                        globals.colorHighlight),
+                                    shape: MaterialStateProperty.all<
+                                            RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ))),
+                                onPressed: () {
+                                  if (_emailController.text.isEmpty ||
+                                      _confirmController.text.isEmpty ||
+                                      _passwordController.text.isEmpty) {
+                                    showAlertDialog(
+                                        context, "Please fill all the fields!");
+                                    return;
+                                  }
+                                  if (_confirmController.text !=
+                                      _passwordController.text) {
+                                    showAlertDialog(
+                                        context, "Passwords do not match!");
+                                    _confirmController.clear();
+                                    _passwordController.clear();
+                                    return;
+                                  }
+                                  Auth().register(_emailController.text.trim(),
+                                      _passwordController.text.trim(), context);
+                                },
+                                child: Text(
+                                  'Register',
+                                  style: globals.buttonFontText,
+                                )),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Expanded (
-                flex: 1,
-                child: Container(
-                ),  
-              )
-            ],
-          ),
-        ),
-      );
+                Expanded(
+                  flex: 1,
+                  child: Container(),
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
